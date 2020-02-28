1a05d2e854cd1ce9f264b4a750c56bd9b489ae2c
Bkaes <bkaes8@gmail.com>
2020-02-27 20:03:12 -0500

    got pp1-react working after re-structure

---
+++ pp1-ef-core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFGetstarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
